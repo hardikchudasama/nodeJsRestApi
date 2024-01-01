6d934f481f14d7f24638877769c0610f3c5d69ca
Hardik Chudasama <hchudasama@datasos.local>
2024-01-01 17:05:08 +0530

    change server config

--- server.js
+++ server.js
@@ -7,12 +7,12 @@ const verifyToken = require('./verifyToken');
 var sql = require("mssql");
 
 const app = express();
-app.use(cors({ origin: '*' }));
+app.use(cors({ origin: 'https://nodejsrestapi.onrender.com'}));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "https://nodejsrestapi.onrender.com");
+  res.header("Access-Control-Allow-Origin", "*");
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
