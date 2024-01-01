1443fe68877bc2613b272cc67fa5ee832a219565
Hardik Chudasama <hchudasama@datasos.local>
2024-01-01 16:58:35 +0530

    change server settings

--- server.js
+++ server.js
@@ -7,7 +7,6 @@ const verifyToken = require('./verifyToken');
 var sql = require("mssql");
 
 const app = express();
-app.use(cors());
 app.use(cors({ origin: '*' }));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
