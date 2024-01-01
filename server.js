d7c07cc953e8419d110c2b800e9608c35ca658f0
Hardik Chudasama <hchudasama@datasos.local>
2024-01-01 17:47:23 +0530

    change config

--- server.js
+++ server.js
@@ -12,15 +12,14 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(cors());
-app.options('*', cors());
-
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "https://nodejsrestapi.onrender.com");
+  res.header("Access-Control-Allow-Origin", "*");
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
+
+app.use(cors());
 
 
 const port = process.env.port || process.env.PORT || 8081;
